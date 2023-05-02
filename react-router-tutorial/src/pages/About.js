ea2e8fdd98d4da704932ae057d04ddcdf099fe0e
James <dnlwlstp123@gmail.com>
2023-05-02 23:45:19 +0900

    react-router-tutorial 추가

---
+++ react-router-tutorial/src/pages/About.js
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+const About = () => {
+  return (
+    <div>
+      <h1>소개</h1>
+      <p>리액트 라우터를 사용해 보는 프로젝트</p>
+      <Link to={"/"}>홈 페이지</Link>
+    </div>
+  );
+};
+export default About;
