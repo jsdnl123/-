dc57439148790eead1df6e0df11a977a14a1956b
James <dnlwlstp123@gmail.com>
2023-05-08 20:39:12 +0900

    useContext 사용하여 Consumer 사용 없이 Context 구현

--- context-tutorial/src/components/ColorBox.js
+++ context-tutorial/src/components/ColorBox.js
@@ -1,27 +1,24 @@
-import ColorContext, { ColorConsumer } from "../contexts/color";
-
+import ColorContext from "../contexts/color";
+import { useContext } from "react";
 const ColorBox = () => {
+  const { state } = useContext(ColorContext);
   return (
-    <ColorConsumer>
-      {({ state }) => (
-        <>
-          <div
-            style={{
-              width: "64px",
-              height: "64px",
-              background: state.color,
-            }}
-          />
-          <div
-            style={{
-              width: "32px",
-              height: "32px",
-              background: state.subcolor,
-            }}
-          ></div>
-        </>
-      )}
-    </ColorConsumer>
+    <>
+      <div
+        style={{
+          width: "64px",
+          height: "64px",
+          background: state.color,
+        }}
+      />
+      <div
+        style={{
+          width: "32px",
+          height: "32px",
+          background: state.subcolor,
+        }}
+      ></div>
+    </>
   );
 };
 export default ColorBox;
